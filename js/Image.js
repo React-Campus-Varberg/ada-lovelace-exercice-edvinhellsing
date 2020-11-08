0b63dd3cb408676f305e2f4387abf04190cc9bdf
edvinhellsing <hello@edvinhellsing.com>
2020-11-08 15:51:33 +0100

    Remove unused props

--- js/Image.js
+++ js/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import imgUrl from '../img/256px-Ada_Lovelace_portrait.jpg';
 
-function AdaImage(props) {
+function AdaImage() {
 
    return(
       <img src={imgUrl} class="image" alt='Portrait of Ada Lovelace' />
